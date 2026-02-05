83348f4e79329d9400317a0726c9e431b36cbe9f
Rajan-4900 <rajan91480@gmail.com>
2026-02-05 13:04:22 +0530

    Database Collection Is Done For Student

--- student/views.py
+++ student/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Student
 
 def index(request):
  return render(request, 'index.html')
@@ -15,12 +16,24 @@ def student(request):
         branch=request.POST.get('branch')
         sem=request.POST.get('sem')
 
-        # printing fetched data on console
-        print("Name :",name)
-        print("USN :",usn)
-        print("email :",email)
-        print("phone :",phone)
-        print("college :",college)
-        print("degree :",degree)
-        print("sem :",sem)
+        
+        stu_obj=Student()
+        stu_obj.name=name
+        stu_obj.usn=usn
+        stu_obj.email=email
+        stu_obj.phone=phone
+        stu_obj.college=college
+        stu_obj.degree=degree
+        stu_obj.branch=branch
+        stu_obj.sem=sem
+        stu_obj.save()
+
+        # printing fetched data on console --> to Comform
+        # print("Name :",name)
+        # print("USN :",usn)
+        # print("email :",email)
+        # print("phone :",phone)
+        # print("college :",college)
+        # print("degree :",degree)
+        # print("sem :",sem)
  return render(request,'student.html')
